caf308319bef3c79bf8536b5d49bc49f0ec1fa54
Naveen Kumar Manchala <100999011+NaveenManchala9698@users.noreply.github.com>
2023-02-05 23:53:31 +0100

    Update Topbar.jsx

--- dashboard/src/components/topbar/Topbar.jsx
+++ dashboard/src/components/topbar/Topbar.jsx
@@ -7,7 +7,7 @@ export default function Topbar() {
     <div className="topbar">
       <div className="topbarWrapper">
         <div className="topLeft">
-          <span className="logo">Naveenadmin</span>
+          <span className="logo">Naveen</span>
         </div>
         <div className="topRight">
           <div className="topbarIconContainer">
