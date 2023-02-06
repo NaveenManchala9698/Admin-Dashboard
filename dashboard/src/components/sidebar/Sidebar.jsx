debeb0f1d39dd190cb4373c0f0ae9069ab7005fa
Naveen Kumar Manchala <100999011+NaveenManchala9698@users.noreply.github.com>
2023-02-06 22:06:29 +0100

    Update Sidebar.jsx

--- dashboard/src/components/sidebar/Sidebar.jsx
+++ dashboard/src/components/sidebar/Sidebar.jsx
@@ -48,7 +48,7 @@ export default function Sidebar() {
               </li>
             </Link>
             <Link to="/products" className="link">
-              <li className="sidebarListItems">
+              <li className="sidebarListItem">
                 <Storefront className="sidebarIcon" />
                 Products
               </li>
