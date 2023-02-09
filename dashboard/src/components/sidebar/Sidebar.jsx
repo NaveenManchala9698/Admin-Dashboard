bb1db15a94c504d81d49dde13fc286f066fb6296
Naveen Kumar Manchala <100999011+NaveenManchala9698@users.noreply.github.com>
2023-02-09 22:09:01 +0100

    Update Sidebar.jsx

--- dashboard/src/components/sidebar/Sidebar.jsx
+++ dashboard/src/components/sidebar/Sidebar.jsx
@@ -59,7 +59,7 @@ export default function Sidebar() {
             </li>
             <li className="sidebarListItem">
               <BarChart className="sidebarIcon" />
-              Reports
+              Report
             </li>
           </ul>
         </div>
